7e280c27cc84aa968bca5be8b318105a32c426cd
Ash_Abera17 <Ash_Abera17@ashenafis-mbp.lan>
2021-04-13 07:42:08 -0400

    Initial Commit

---
+++ src/exercisetwo/Question1.java
@@ -0,0 +1,15 @@
+package exercisetwo;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		//concatenate
+		String greeting = "Hello";
+		String st1 = "World";
+		
+		System.out.println(greeting + " " + st1);
+
+	}
+
+}
